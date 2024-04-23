3031dbd6459c30879c880f86bfb70d988bda0196
YungRui <75406355+YungRui0402@users.noreply.github.com>
2024-04-23 17:29:08 +0800

    Time: 9 ms (62.13%) | Memory: 10.2 MB (91.98%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if(numRows==1 || s.length()<=numRows) return s;
+
+        int movetotal = 2*numRows-2;
+        string ans="";
+        int step1=movetotal,step2=0;
+
+        for(int i=0;i<numRows;i++){
+            ans+=s[i];
+            for(int j=i;j<s.length();j+=movetotal){
+                if(j+step1<s.length()&&step1!=0)
+                    ans+=s[j+step1];
+                if(j+step1+step2<s.length()&&step2!=0)
+                    ans+=s[j+step1+step2];
+            }
+            step1-=2;
+            step2+=2;
+
+        }
+        return ans;
+    }
+};
