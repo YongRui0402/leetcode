4ecbb5d47c1b2af83b7396b95286597f9d1f0f67
YungRui <75406355+YungRui0402@users.noreply.github.com>
2024-04-26 23:15:10 +0800

    Time: 24 ms (91.03%) | Memory: 16.9 MB (86.55%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(), res = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        for(int j = 0; j < n; ++j) {
-            dp[0][j] = grid[0][j];
-        }
-
-        for(int i = 1; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                int temp = INT_MAX;
-
-                for(int k = 0; k < n; ++k) {
-                    if(k != j) {
-                        temp = min(temp, grid[i][j] + dp[i - 1][k]);
-                    }
-
-                    dp[i][j] = temp;
+        int n=grid[0].size();
+        if(n==1)
+            return grid[0][0];
+        for(int i=1;i<n;i++){
+            int m1=100*250,i1=-1,m2=100*250,i2=-1;
+            for(int j=0;j<n;j++){
+                if(grid[i-1][j]<m1){
+                    m2=m1;
+                    i2=i1;
+                    m1=grid[i-1][j];
+                    i1=j;
+                }
+                else if(grid[i-1][j]<m2){
+                    m2=grid[i-1][j];
+                    i2=j;
                 }
             }
+            // cout<<i1<<" "<<m1<<" "<<i2<<" "<<m2<<" "<<endl;
+            for(int j=0;j<n;j++){
+                if(j!=i1)
+                    grid[i][j]+=m1;
+                else
+                    grid[i][j]+=m2;
+            }
         }
-
-        for(int j = 0; j < n; ++j) {
-            res = min(res, dp[n - 1][j]);
+        int ans=100*250;
+        for(int j=0;j<n;j++){
+            ans=min(grid[n-1][j],ans);
         }
-
-        return res;
+        return ans;
     }
 };
